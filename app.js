5a23aa7912e130fbb5b134700525473ef02b54e7
BuyanLi <buyanli95@gmail.com>
2018-03-26 23:07:41 +1100

    prepare deploying

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const router = require('./routes/routes');
 
+const PORT = process.env.PORT || 3000;
+
 app.set("view engine", 'ejs');
 
 //allow app to make use of routes, the router is exported from the routes file
 app.use('/', router);
 
-app.listen(3000, function(){
-    console.log("server start at port 3000");
+// app.listen(3000, function(){
+//     console.log("server start at port 3000");
+// });
+
+app.listen(PORT, function(){
+    console.log(`Express listening on port ${PORT}`);
 });
