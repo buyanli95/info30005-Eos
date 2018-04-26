3218814c4582dec08217c024ad226e474f4448bc
BuyanLi <buyanli95@gmail.com>
2018-04-26 12:59:39 +1000

    db connected to localhost

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ const router = require('./routes/routes');
 const mongodb = require('mongodb');
 let uri = 'mongodb://eosdev:info30005@ds259119.mlab.com:59119/eosdb';
 
-mongodb.MongoClient.connect(uri, function(err, client){
-    if(err) throw err;
+mongodb.MongoClient.connect(uri, function(err, client) {
+    if (err) throw err;
     let db = client.db('eosdb');
-    db.once('open', function () {
-        console.log('connect mongo')
-    });
     let participants = db.collection('participants');
     let participant_data = [
         {
-            fullName : 'par',
+            fullName: 'par',
             email: 'par@gmail.com',
             password: 'pass'
         },
@@ -25,58 +22,19 @@ mongodb.MongoClient.connect(uri, function(err, client){
             password: 'pass'
         }
     ];
+    //write data
     participants.insert(participant_data, function (err, result) {
-        if(err) throw err;
+        if (err) throw err;
     })
-
-
-
-    //creat seed data for test
-    // let seedData = [
-    // {
-    //     decades: '1990s',
-    //     song: 'you light up my life'
-    // }];
-    // let songs = db.collection('songs');
-    // songs.insert(seedData, function (err, result) {
-    //     if(err) throw err;
-    // })
-
-    //test insert to exsiting collection 'test'
-    // db.collection('test').insertOne(
-    //     {
-    //         title:'hello'
-    //     },
-    //     function(err, res){
-    //         if(err){
-    //             db.close();
-    //             return console.log(err);
-    //         }
-    //     }
-    // )
+    //read dara
+    participants.find(
+        {
+            fullName: 'par'
+        }).toArray(function (err, docs) {
+        console.log(docs[0]);
+    })
 })
 
-//db2
-// const MongoClient = require('mongodb').MongoClient;
-// const Mongo_URL = 'mongodb://eosdev:info30005@ds259119.mlab.com:59119/eosdb';
-// MongoClient.connect(Mongo_URL, (err, db) =>{
-//     if(err){
-//         return console.log(err);
-//     }
-//     db.collection('test').insertOne(
-//         {
-//             title: "hello mongodb",
-//             text: 'works?'
-//         },
-//         function(err, res){
-//             if(err){
-//                 db.close();
-//                 return console.log(err);
-//             }
-//             db.close();
-//         }
-//     )
-// });
 
 
 const PORT = process.env.PORT || 3000;
