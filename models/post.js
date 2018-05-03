109d516ec332be0c120107541a405ca803c390b4
BuyanLi <buyanli95@gmail.com>
2018-05-03 14:36:59 +1000

    modify post model

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+//post Schema
+const PostSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    brief:{
+        type: String,
+        required:true
+    },
+    detail:{
+        type: String,
+        required: true
+    },
+    // like:{
+    //     type: Boolean
+    // },
+    // join:{
+    //     type: Boolean
+    // }
+})
